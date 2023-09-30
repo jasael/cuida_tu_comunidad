701ffc629cff8c28cbb3284386c0370478950b10
Jasael Palma <programadorjp2001@gmail.com>
2023-09-30 00:35:08 -0600

    adding CRUD functionality

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -5,23 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ProjectController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $state = $request->query('state');
+        $title = $request->query('title');
+        $offset = $request->query('offset', 0);
+        $limit = $request->query('limit', 9);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $projects = Project::query()
+            ->when($state, function ($query, $state) {
+                return $query->whereRaw('state = ?', [$state]);
+            })
+            ->when($title, function ($query, $title) {
+                return $query->whereRaw('title ilike ?', ['%' . $title . '%']);
+            })
+            ->orderBy('created_at', 'desc')
+            ->offset($offset)
+            ->limit($limit)
+            ->get();
+
+        return response()->json([
+            'projects' => $projects
+        ], 200);
     }
 
     /**
@@ -29,38 +43,72 @@ class ProjectController extends Controller
      */
     public function store(StoreProjectRequest $request)
     {
-        //
-    }
+        $validated = $request->validated();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Project $project)
-    {
-        //
-    }
+        Project::create($validated);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Project $project)
-    {
-        //
+        return response()->json([
+            'message' => 'Proyecto creado exitosamente',
+        ], 201);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProjectRequest $request, Project $project)
+    public function update(UpdateProjectRequest $request, int $id)
     {
-        //
+        $validated = $request->validated();
+
+        try {
+            $project = Project::findOrFail($id);
+
+            $project->update($validated);
+
+            return response()->json([
+                'message' => 'Proyecto actualizado exitosamente',
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Proyecto no encontrado',
+            ], 404);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error al actualizar el proyecto',
+            ], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Project $project)
+    public function destroy(int $id)
     {
-        //
+        try {
+            $project = Project::findOrFail($id);
+
+            if ($project->likes > 0) {
+                throw ValidationException::withMessages([
+                    'message' => 'No se puede eliminar un proyecto con likes',
+                ]);
+            }
+
+            $project->delete();
+
+            return response()->json([
+                'message' => 'Proyecto eliminado exitosamente',
+            ], 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Proyecto no encontrado',
+            ], 404);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'No se puede eliminar un proyecto con likes',
+            ], 400);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Error al eliminar el proyecto',
+            ], 500);
+        }
     }
 }
