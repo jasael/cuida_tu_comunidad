a1c5b290b700b2667841b03a18f313229bcb17df
Jasael Palma <programadorjp2001@gmail.com>
2023-09-30 00:01:46 -0600

    adding validation request

--- laravel/app/Http/Requests/StoreProjectRequest.php
+++ laravel/app/Http/Requests/StoreProjectRequest.php
@@ -11,7 +11,7 @@ class StoreProjectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,34 @@ class StoreProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:20',
+            'description' => 'required|string|max:500',
+            'date' => 'required|date',
+            'author' => 'required|string|max:20',
+            'state' => 'required|string',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'El título es requerido',
+            'title.string' => 'El título debe ser un texto',
+            'title.max' => 'El título debe tener como máximo 20 caracteres',
+            'description.required' => 'La descripción es requerida',
+            'description.string' => 'La descripción debe ser un texto',
+            'description.max' => 'La descripción debe tener como máximo 500 caracteres',
+            'date.required' => 'La fecha es requerida',
+            'date.date' => 'La fecha debe ser una fecha',
+            'author.required' => 'El autor es requerido',
+            'author.string' => 'El autor debe ser un texto',
+            'author.max' => 'El autor debe tener como máximo 20 caracteres',
+            'state.required' => 'El estado es requerido',
+            'state.string' => 'El estado debe ser un texto',
         ];
     }
 }
